1834de0c624dc5a1b85666a0515256707f89820d
Preston Bateman <prestonbbateman@gmail.com>
2024-03-11 20:44:18 -0500

    db changes from init

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const app = express()
+const mongodb = require('./data/database')
 
 const port = process.env.PORT || 3000
 
 app.use('/', require('./routes'))
 
-app.listen(port, () => {console.log(`Running on port ${port}`)})
+mongodb.initDb((err) => {
+    if(err){
+        console.log(err)
+    }else {
+        app.listen(port, () => {console.log(`Database is listening. Running on port ${port}`)})
+    }
+})
