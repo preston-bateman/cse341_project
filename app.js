d57ebc11c99ff7da6f890113915f7fd3d461389a
Preston Bateman <prestonbbateman@gmail.com>
2024-03-20 14:49:21 -0500

    added rest client local functionality

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const app = express()
 const mongodb = require('./data/database')
+const bodyParser = require('body-parser')
 
 const port = process.env.PORT || 3000
 
-app.use('/', require('./routes'))
 app.use(bodyParser.json())
+app.use('/', require('./routes'))
+
 
 mongodb.initDb((err) => {
     if(err){
